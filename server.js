2df31e68c74ca15c1f72399f29ba96b7ae541f74
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-08-17 19:31:17 -0500

    test

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
